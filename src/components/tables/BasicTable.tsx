92b9fc60db3d26f503c65a3ee7391497c7d0a212
guanjinli <wb_guanjinli@zhongan.com>
2023-02-14 14:48:14 +0800

    test

--- src/components/tables/BasicTable.tsx
+++ src/components/tables/BasicTable.tsx
@@ -1,3 +1,11 @@
+/*
+ * @Author: jinli
+ * @Date: 2023-02-14 09:41:37
+ * @LastEditTime: 2023-02-14 14:42:51
+ * @LastEditors: jinli
+ * @Description: 
+ * @FilePath: \reactmaster\src\components\tables\BasicTable.tsx
+ */
 /**
  * Created by hao.cheng on 2017/4/15.
  */
@@ -6,58 +14,58 @@ import { Table, Button } from 'antd';
 import { DownOutlined } from '@ant-design/icons';
 
 const columns = [
-    {
-        title: 'Name',
-        dataIndex: 'name',
-        key: 'name',
-        render: (text: any) => <span>{text}</span>,
-    },
-    {
-        title: 'Age',
-        dataIndex: 'age',
-        key: 'age',
-    },
-    {
-        title: 'Address',
-        dataIndex: 'address',
-        key: 'address',
-    },
-    {
-        title: 'Action',
-        key: 'action',
-        render: (text: any, record: any) => (
-            <span>
-                <Button>Action 一 {record.name}</Button>
-                <span className="ant-divider" />
-                <Button>Delete</Button>
-                <span className="ant-divider" />
-                <Button className="ant-dropdown-link">
-                    More actions <DownOutlined />
-                </Button>
-            </span>
-        ),
-    },
+  {
+    title: 'Name1',
+    dataIndex: 'name',
+    key: 'name',
+    render: (text: any) => <span>{text}</span>,
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (text: any, record: any) => (
+      <span>
+        <Button>Action 一 {record.name}</Button>
+        <span className="ant-divider" />
+        <Button>Delete</Button>
+        <span className="ant-divider" />
+        <Button className="ant-dropdown-link">
+          More actions <DownOutlined />
+        </Button>
+      </span>
+    ),
+  },
 ];
 
 const data = [
-    {
-        key: '1',
-        name: 'John Brown',
-        age: 32,
-        address: 'New York No. 1 Lake Park',
-    },
-    {
-        key: '2',
-        name: 'Jim Green',
-        age: 42,
-        address: 'London No. 1 Lake Park',
-    },
-    {
-        key: '3',
-        name: 'Joe Black',
-        age: 32,
-        address: 'Sidney No. 1 Lake Park',
-    },
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+  },
 ];
 
 const BasicTable = () => <Table columns={columns} dataSource={data} />;
